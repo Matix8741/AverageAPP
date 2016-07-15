0bc22d6f23433c1e4a493eaa5121dc1d244e26b2
Matix8741 <matix8741@gmail.com>
2016-07-15 12:24:51 +0200

    add some static field for marks

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,15 +3,25 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 
 public class Main extends Application {
+	HBox hbox;
+	TextField textfields[];
+	Scene scene;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			hbox = new HBox();
+			textfields = new TextField[4];
+			for(TextField test : textfields) {
+				test = new TextField();
+				hbox.getChildren().add(test);
+			}
+			hbox.setSpacing(10);
+			scene = new Scene(hbox,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
